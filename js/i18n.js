38149c7aa8ca380c6a1eaa4f9b52be308e3b3548
JINLING1 <1124290657@qq.com>
2025-10-10 20:53:19 +0800

    格式化所有文档

--- js/i18n.js
+++ js/i18n.js
@@ -1,53 +1,47 @@
 const i18n = {
-  currentLang:'en',
-  translations:{},
+	currentLang: 'en',
+	translations: {},
 
-  async loadLanguage(newLang){
-  try{
-    const response = await fetch(`../i18n/${newLang}.json`);
-    const data = await response.json();
-    this.translations = data;
-    this.currentLang = newLang;
-    this.updateTexts();
-    localStorage.setItem('lang',newLang);
-  }
-  catch(err){
-    console.error('Failed to load language file:', err);
-  }
-},    
+	async loadLanguage(newLang) {
+		try {
+			const response = await fetch(`../i18n/${newLang}.json`);
+			const data = await response.json();
+			this.translations = data;
+			this.currentLang = newLang;
+			this.updateTexts();
+			localStorage.setItem('lang', newLang);
+		} catch (err) {
+			console.error('Failed to load language file:', err);
+		}
+	},
 
-updateTexts(){
-  document.querySelectorAll('[data-i18n]').forEach(el=>{
-    const key = el.getAttribute('data-i18n');
-    if(this.translations[key]){    
-      el.style.opacity = 0;
-      console.log(el.style.opacity);
-      setTimeout(()=>{
-        el.textContent = this.translations[key];
-        el.style.opacity = 1;
-      },100);
-    }
-  });
-},
+	updateTexts() {
+		document.querySelectorAll('[data-i18n]').forEach((el) => {
+			const key = el.getAttribute('data-i18n');
+			if (this.translations[key]) {
+				el.style.opacity = 0;
+				console.log(el.style.opacity);
+				setTimeout(() => {
+					el.textContent = this.translations[key];
+					el.style.opacity = 1;
+				}, 100);
+			}
+		});
+	},
 
-init(){
-  const savedLang = localStorage.getItem('lang')||this.currentLang;
-  this.loadLanguage(savedLang);
-}
+	init() {
+		const savedLang = localStorage.getItem('lang') || this.currentLang;
+		this.loadLanguage(savedLang);
+	},
 };
 
 i18n.init();
-document.querySelector('.js-switch-zh').addEventListener('click',()=>{
-  console.log('Switch to Chinese');
-  i18n.loadLanguage('zh');
-})
+document.querySelector('.js-switch-zh').addEventListener('click', () => {
+	console.log('Switch to Chinese');
+	i18n.loadLanguage('zh');
+});
 
-document.querySelector('.js-switch-en').addEventListener('click',()=>{
-  console.log('Switch to English');
-  i18n.loadLanguage('en');
-})
-
-
-
-
-
+document.querySelector('.js-switch-en').addEventListener('click', () => {
+	console.log('Switch to English');
+	i18n.loadLanguage('en');
+});
